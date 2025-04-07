c307542c244df7e00c9b2d3b7a3f710b8febba3b
Marcin Bajkowski <129612167+LeafCodeX@users.noreply.github.com>
2025-04-07 15:44:03 +0200

    fix: enable running from console

--- src/app/backend/main.py
+++ src/app/backend/main.py
@@ -1,6 +1,11 @@
 import sys
+from pathlib import Path
 from PySide6.QtWidgets import QApplication
 from icecream import ic
+
+current_file = Path(__file__).resolve()
+project_root = current_file.parents[3]
+sys.path.append(str(project_root))
 
 from src.app.frontend.windows import MainWindow
 
